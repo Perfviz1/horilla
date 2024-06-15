1900a84a8fa5da279c0c9dd8e5ea8211c8350ce6
Perfviz1 <perfviz@gmail.com>
2024-06-15 21:09:13 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -22,4 +22,20 @@ def main():
 if __name__ == "__main__":
     main()
 
-runserver.DEFAULT_PORT = os.environ.get('PORT', '8000')
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
+    # Get the port from the environment variable
+    port = os.environ.get('PORT', '8000')
+    sys.argv += ['runserver', '0.0.0.0:' + port]
+
+    execute_from_command_line(sys.argv)
